ed05031f198ce8f6acd2a11699349b5fb5be2cf6
dehmer <horst.dehmer@syncpoint.io>
2024-07-29 07:40:51 +0200

    benchmark: clean layout.

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -53,127 +53,184 @@ await bench.mark('### JIT COMPILER WARM-UP (ignore) ###', 10000, () => {
   }
 })
 
-// of :: Signal s => () -> s
-await bench.mark('[80cd]', 10000, () => {
-  assert.strictEqual(Signal.of()(), undefined)
-})
+const suite = []
 
-// of :: Signal s => v -> s v
-await bench.mark('[4e38]', 10000, () => {
-  assert.strictEqual(Signal.of(10)(), 10)
-})
-
-// link :: Signal s => (a -> b) -> s a -> s b
-await bench.mark('[a642]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.link(a => a + 2, a)
-  assert.strictEqual(b(), 3)
-})
-
-// link :: Signal s => (...[*] -> b) -> [s *] -> s b
-await bench.mark('[3155]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.of(2)
-  const c = Signal.link((a, b) => a + b, [a, b])
-  assert.strictEqual(c(), 3)
-})
-
-// set :: Signal s => v -> s v
-await bench.mark('[4568]', 10000, () => {
-  const a = Signal.of(1)
-  a(2)
-  assert.strictEqual(a(), 2)
-})
-
-// link :: Signal s => (...[*] -> b) -> [s *] -> s b
-await bench.mark('[9a8c]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.link(a => a + 1, a)
-  const c = Signal.link(b => b + 1, b)
-  a(2)
-  assert.strictEqual(c(), 4)
-})
-
-// link :: Signal s => (...[*] -> b) -> [s *] -> s b
-await bench.mark('[44b2]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.link(a => a + 1, a)
-  const c = Signal.link(b => b + 1, b)
-  const d = Signal.link(c => c + 1, c)
-  a(2)
-  assert.strictEqual(d(), 5)
-})
-
-// link :: Signal s => (...[*] -> b) -> [s *] -> s b
-await bench.mark('[96f2]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.link(a => a + 1, a)
-  const c = Signal.link(b => b + 1, b)
-  const d = Signal.link(c => c + 1, c)
-  const e = Signal.link(d => d + 1, d)
-  a(2)
-  assert.strictEqual(e(), 6)
-})
-
-// diamond
-await bench.mark('[ee70]', 10000, () => {
-  const a = Signal.of(1)
-  const b = Signal.link(a => a + 1, a)
-  const c = Signal.link(a => a + 2, a)
-  const d = Signal.link((b, c) => b * c, [b, c])
-  a(2)
-  assert.strictEqual(d(), 12)
-})
-
-// diamond/extended
-await bench.mark('[bd93]', 10000, () => {
-  const a = Signal.of()
-  const b = Signal.link(a => a + 1, [a])
-  const c = Signal.link(a => a + 2, [a])
-  const d = Signal.link(c => c + 3, [c])
-  const e = Signal.link((b, d) => b + d, [b, d])
-  const f = Signal.scan(R.flip(R.append), [], e)
-  ;[1, 5, 11, 5, 1].forEach(a)
-  assert.deepStrictEqual(f(), [8, 16, 28, 16, 8])
-})
-
-// 16-bit adder
-await bench.mark('[62cb]', 100, () => {
-  const i0 = Math.ceil(Math.random() * 32768)
-  const i1 = Math.ceil(Math.random() * 32768)
-  const { a, b, s, cout } = parallelAdder(Signal.of(0))
-  encode(i0).forEach((v, i) => a[i](v))
-  encode(i1).forEach((v, i) => b[i](v))
-  const actual = decode([...s.map(s => s()), cout()])
-  assert.strictEqual(actual, i0 + i1)
-})
-
-// matrix (4 x 20)
-await bench.mark('[4357]', 10, () => {
-  // Currently depths of 30 and up cannot be handled because of an issue in increase/decrease.
-  const depth = 30
-  const expected = {
-    10: [2, 4, -2, -3],
-    15: [-1, -2, 3, 4],
-    20: [-2, 1, -4, -4],
-    25: [3, 2, 4, 2],
-    30: [-4, -3, -2, -1],
-    35: [4, 4, 1, -2]
+suite.push({
+  id: '[80cd]',
+  label: 'of :: Signal s => () -> s',
+  cycles: 10000,
+  fn: () => {
+    assert.strictEqual(Signal.of()(), undefined)
   }
-
-  const layers = R.range(0, depth).reduce((acc, i) => {
-    acc.push([
-      Signal.link(b => b, acc[i][1]),
-      Signal.link((a, c) => a - c, [acc[i][0], acc[i][2]]),
-      Signal.link((b, d) => b + d, [acc[i][1], acc[i][3]]),
-      Signal.link(c => c, acc[i][2])
-    ])
-    return acc
-  }, [[Signal.of(1), Signal.of(2), Signal.of(3), Signal.of(4)]])
-
-  ;[4, 3, 2, 1].forEach((v, i) => R.head(layers)[i](v))
-  const actual = R.last(layers).map(fn => fn())
-  assert.deepStrictEqual(actual, expected[depth])
 })
+
+suite.push({
+  id: '[4e38]',
+  label: 'of :: Signal s => v -> s v',
+  cycles: 10000,
+  fn: () => {
+    assert.strictEqual(Signal.of(10)(), 10)
+  }
+})
+
+suite.push({
+  id: '[a642]',
+  label: 'link :: Signal s => (a -> b) -> s a -> s b',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.link(a => a + 2, a)
+    assert.strictEqual(b(), 3)
+  }
+})
+
+suite.push({
+  id: '[3155]',
+  label: 'link :: Signal s => (...[*] -> b) -> [s *] -> s b',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.of(2)
+    const c = Signal.link((a, b) => a + b, [a, b])
+    assert.strictEqual(c(), 3)
+  }
+})
+
+suite.push({
+  id: '[4568]',
+  label: 'set :: Signal s => v -> s v',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    a(2)
+    assert.strictEqual(a(), 2)
+  }
+})
+
+suite.push({
+  id: '[9a8c]',
+  label: 'link :: Signal s => (...[*] -> b) -> [s *] -> s b',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.link(a => a + 1, a)
+    const c = Signal.link(b => b + 1, b)
+    a(2)
+    assert.strictEqual(c(), 4)
+  }
+})
+
+suite.push({
+  id: '[44b2]',
+  label: 'link :: Signal s => (...[*] -> b) -> [s *] -> s b',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.link(a => a + 1, a)
+    const c = Signal.link(b => b + 1, b)
+    const d = Signal.link(c => c + 1, c)
+    a(2)
+    assert.strictEqual(d(), 5)
+  }
+})
+
+suite.push({
+  id: '[96f2]',
+  label: 'link :: Signal s => (...[*] -> b) -> [s *] -> s b',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.link(a => a + 1, a)
+    const c = Signal.link(b => b + 1, b)
+    const d = Signal.link(c => c + 1, c)
+    const e = Signal.link(d => d + 1, d)
+    a(2)
+    assert.strictEqual(e(), 6)
+  }
+})
+
+suite.push({
+  id: '[ee70]',
+  label: 'diamond',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of(1)
+    const b = Signal.link(a => a + 1, a)
+    const c = Signal.link(a => a + 2, a)
+    const d = Signal.link((b, c) => b * c, [b, c])
+    a(2)
+    assert.strictEqual(d(), 12)
+  }
+})
+
+suite.push({
+  id: '[bd93]',
+  label: 'diamond/extended',
+  cycles: 10000,
+  fn: () => {
+    const a = Signal.of()
+    const b = Signal.link(a => a + 1, [a])
+    const c = Signal.link(a => a + 2, [a])
+    const d = Signal.link(c => c + 3, [c])
+    const e = Signal.link((b, d) => b + d, [b, d])
+    const f = Signal.scan(R.flip(R.append), [], e)
+    ;[1, 5, 11, 5, 1].forEach(a)
+    assert.deepStrictEqual(f(), [8, 16, 28, 16, 8])
+  }
+})
+
+suite.push({
+  id: '[62cb]',
+  label: '16-bit adder',
+  cycles: 100,
+  fn: () => {
+    const i0 = Math.ceil(Math.random() * 32768)
+    const i1 = Math.ceil(Math.random() * 32768)
+    const { a, b, s, cout } = parallelAdder(Signal.of(0))
+    encode(i0).forEach((v, i) => a[i](v))
+    encode(i1).forEach((v, i) => b[i](v))
+    const actual = decode([...s.map(s => s()), cout()])
+    assert.strictEqual(actual, i0 + i1)
+  }
+})
+
+suite.push({
+  id: '[4357]',
+  label: 'matrix (4 x 20)',
+  cycles: 10,
+  fn: () => {
+    // Currently depths of 30 and up cannot be handled because of an issue in increase/decrease.
+    const depth = 30
+    const expected = {
+      10: [2, 4, -2, -3],
+      15: [-1, -2, 3, 4],
+      20: [-2, 1, -4, -4],
+      25: [3, 2, 4, 2],
+      30: [-4, -3, -2, -1],
+      35: [4, 4, 1, -2]
+    }
+
+    const layers = R.range(0, depth).reduce((acc, i) => {
+      acc.push([
+        Signal.link(b => b, acc[i][1]),
+        Signal.link((a, c) => a - c, [acc[i][0], acc[i][2]]),
+        Signal.link((b, d) => b + d, [acc[i][1], acc[i][3]]),
+        Signal.link(c => c, acc[i][2])
+      ])
+      return acc
+    }, [[Signal.of(1), Signal.of(2), Signal.of(3), Signal.of(4)]])
+
+    ;[4, 3, 2, 1].forEach((v, i) => R.head(layers)[i](v))
+    const actual = R.last(layers).map(fn => fn())
+    assert.deepStrictEqual(actual, expected[depth])
+  }
+})
+
+await Promise.all(suite.map(({ id, cycles, fn }) => {
+  return bench.mark(id, cycles, fn)
+}))
 
 bench.utils.logMem()
+
+console.log('\nLegend:')
+suite.forEach(({ id, label }) => console.log(id, label))
